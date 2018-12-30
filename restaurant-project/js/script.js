d0c317e45cd741a374e83ee3f7e540aeb62e12b2
lifeLessCoder <ankushshetkar16@gmail.com>
2018-12-30 10:11:51 +0530

    minor-edit

--- restaurant-project/js/script.js
+++ restaurant-project/js/script.js
@@ -13,7 +13,7 @@ $(function() { // Same as document.addEventListener('DOMContentLoaded', ... );
 
 	var dc = {};
 
-	var homeHtml = '/snippets/home-snippet.html';
+	var homeHtml = 'snippets/home-snippet.html';
 	var allCategoriesUrl = 'http://davids-restaurant.herokuapp.com/categories.json';
 	var categoriesTitleHtml = 'snippets/categories-title-snippet.html';
 	var categoryHtml = 'snippets/category-snippet.html';
